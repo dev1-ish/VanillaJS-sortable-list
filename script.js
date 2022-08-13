1625ff161d1ac9a332d8491d4beb0e16976e46c9
dev1-ish <alma.miguez30@gmail.com>
2022-08-12 23:13:56 -0300

    [update] Drag and Drop -switch functionality

--- script.js
+++ script.js
@@ -52,19 +52,36 @@ function createList() {
 
 function dragStart() {
     // console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+    console.log(dragStartIndex);
 }
 
 function dragEnter() {
     // console.log('Event: ', 'dragenter');
+    this.classList.add('over');
 }
 function dragLeave() {
     // console.log('Event: ', 'dragleave');
+    this.classList.remove('over');
 }
-function dragOver() {
+function dragOver(e) {
     // console.log('Event: ', 'dragover');
+    e.preventDefault();
 }
 function dragDrop() {
     // console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
 }
 
 function addEventListeners() {
